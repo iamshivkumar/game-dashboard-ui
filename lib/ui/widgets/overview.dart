79051f3a362d236d1d8a3843cd27d23c03c5fd87
ShivkumarKonade <shivkumar.konade@virtoustack.com>
2022-06-19 17:39:03 +0530

    first commit

--- lib/ui/widgets/overview.dart
+++ lib/ui/widgets/overview.dart
@@ -1,10 +1,7 @@
-import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:game_dashboard/ui/models/character.dart';
 import 'package:game_dashboard/ui/widgets/my_card.dart';
-import 'package:game_dashboard/utils/assets.dart';
 import 'package:game_dashboard/utils/pallate.dart';
 
 import 'progress_clipper.dart';
